76393a0f98a575ed4f1625f6b83a66a220f655aa
John Mejía Hernández <johnmejiahernandez@Johns-iMac.local>
2019-01-02 20:25:54 -0500

    Added boxes and history components

---
+++ src/components/boxes/week-box.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function WeekBox() {
+    return (
+        <div className="">
+            <div>
+                <span>Week</span>
+                <span>35</span>
+            </div>
+            <div>
+                <span>12 / Sep / 2018</span>
+                <span>12 / Sep / 2018</span>
+            </div>
+        </div>
+    )
+}
+
+
+export default WeekBox;
---
+++ src/components/history/history-layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// Components
+import WeekBox from '../boxes/week-box';
+
+function History(props) {
+    return (
+        <section>
+            <header>
+                <h4>Work History</h4>
+            </header>
+            <WeekBox />
+        </section>
+    )
+}
+
+export default History;
